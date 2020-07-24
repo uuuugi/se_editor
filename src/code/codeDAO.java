e552f2c6f350b0dd465467b50184866a0348685d
안병욱 <@DESKTOP-43H6EG7>
2020-07-25 01:05:48 +0900

    database file add

--- src/code/codeDAO.java
+++ src/code/codeDAO.java
@@ -15,7 +15,7 @@ public class codeDAO {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			// 드라이버 로딩
-			String url = "jdbc:mysql://localhost/fuck?serverTimezone=UTC";
+			String url = "jdbc:mysql://localhost/seEditor?serverTimezone=UTC";
 			// DB url 설정 및 시간설정
 			// ?serverTimezone=UTC시간설정부분
 			conn = DriverManager.getConnection(url, "root", "1324");
