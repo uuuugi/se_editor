015648e23edd755784a93e66a273471445cd96bd
uuuugi <ambyeong99@hs.ac.kr>
2020-08-18 12:52:06 +0900

    게시판 java File 및 DB update

---
+++ src/bulletinBoard/forPostList.java
@@ -0,0 +1,18 @@
+package bulletinBoard;
+
+public class forPostList {//PostList를 위한 class
+	String name;
+	int num;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+}
